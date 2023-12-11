b886e54607f0609a8d5f4486ff424b6d06bb8be0
Ben Kogan <benkogan9@gmail.com>
2023-12-11 01:41:53 -0500

    11, much better

--- 11.py
+++ 11.py
@@ -1,73 +1,53 @@
-from collections import defaultdict, deque
-import heapq
-from aoc import in_bounds, read_input
+import itertools
+from aoc import read_input
 
 lines = read_input()
 
 R = len(lines)
 C = len(lines[0])
-inb = in_bounds(C, R)
+
+galaxies = set()
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == "#":
+            galaxies.add((j, i))
+
+expanded_rows = set()
+expanded_cols = set()
+for row, line in enumerate(lines):
+    galaxy_in_row = False
+    for c in line:
+        if c == "#":
+            galaxy_in_row = True
+            break
+    if not galaxy_in_row:
+        expanded_rows.add(row)
+
+for col in range(C):
+    galaxy_in_col = False
+    for row in lines:
+        if row[col] == "#":
+            galaxy_in_col = True
+            break
+    if not galaxy_in_col:
+        expanded_cols.add(col)
 
 
 def get_total(expansion):
-    adj = {}  # position to list of (neighbor, weight)
-    galaxies = set()
-
-    for i, line in enumerate(lines):
-        for j, c in enumerate(line):
-            if c == "#":
-                galaxies.add((j, i))
-
-            galaxy_in_row = False
-            for o in line:
-                if o == "#":
-                    galaxy_in_row = True
-                    break
-
-            galaxy_in_col = False
-            for o in lines:
-                if o[j] == "#":
-                    galaxy_in_col = True
-                    break
-
-            adjs = []
-            for dx, dy in [(0, 1), (0, -1)]:
-                n = (j + dx, i + dy)
-                if inb(n):
-                    adjs.append((n, 1 if galaxy_in_row else expansion))
-
-            for dx, dy in [(1, 0), (-1, 0)]:
-                n = (j + dx, i + dy)
-                if inb(n):
-                    adjs.append((n, 1 if galaxy_in_col else expansion))
-
-            adj[(j, i)] = adjs
-
     total = 0
-    seen_ends = set()
-    for galaxy in galaxies:
-        seen_ends.add(galaxy)
-        queue = [(0, galaxy)]
-        weights = defaultdict(lambda: float("inf"))
-        weights[galaxy] = 0
-        seen = set()
-        while len(queue) > 0:
-            steps, elt = heapq.heappop(queue)
-            if elt in seen:
-                continue
-            else:
-                seen.add(elt)
-            if elt in galaxies and elt not in seen_ends:
-                total += steps
-            for n, w in adj[elt]:
-                if n not in seen:
-                    new_cost = steps + w
-                    if new_cost < weights[n]:
-                        weights[n] = new_cost
-                        heapq.heappush(queue, (new_cost, n))
+
+    for (sx, sy), (ex, ey) in itertools.combinations(galaxies, 2):
+        man_dist = abs(sx - ex) + abs(sy - ey)
+        in_rows = [
+            row for row in expanded_rows if row in range(min(sy, ey) + 1, max(sy, ey))
+        ]
+        in_cols = [
+            col for col in expanded_cols if col in range(min(sx, ex) + 1, max(sx, ex))
+        ]
+        total += man_dist + ((len(in_rows) + len(in_cols)) * (expansion - 1))
 
     return total
 
 
 print(get_total(2))
-print(get_total(1000000))
+print(get_total(1_000_000))
